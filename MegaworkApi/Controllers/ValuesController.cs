fcbf1539d28f54009cdea437a7c075bdc54de3bb
Douglas Medeiros de Lima <lima.mdouglas@gmail.com>
2021-03-07 19:44:40 -0300

    cmo

---
+++ MegaworkApi/Controllers/ValuesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dominio;
+using ProjetoWebApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+
+        public ValuesController(DataContext contextaux)
+        {
+            _context = contextaux;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var resultado = await _context.Bancos.ToListAsync();
+                return Ok(resultado);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(500, "Banco de dados falhou");
+            }
+            
+        }
+
+        [HttpGet("buscar")]
+        public ActionResult GetBuscar()
+        {
+            var listaBancos = (from banco in _context.Bancos
+                               select banco).ToList();
+            return Ok(listaBancos);
+        }
+
+        [HttpGet("filtro/{nomeBanco}")]
+        public ActionResult GetFiltro(string nomeBanco)
+        {
+            var listaBancos = (from banco in _context.Bancos
+                               where banco.Nome.Contains(nomeBanco)
+                               select banco).ToList();
+            return Ok(listaBancos);
+        }
+
+        [HttpGet("{nomeBanco}")]
+        public ActionResult GetInserir(string nomeBanco)
+        {
+            var banco = new Banco { Nome = nomeBanco};
+                _context.Bancos.Add(banco);
+                _context.SaveChanges();
+        return Ok();
+        }
+
+        [HttpGet("atualizar/{nomeBanco}")]
+        public ActionResult GetAtualizar(string nomeBanco)
+        {
+            var auxBanco = (from banco in _context.Bancos
+                               where banco.Nome == nomeBanco
+                               select banco).FirstOrDefault();
+            auxBanco.Nome = "Santader";
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("deletar/{nomeBanco}")]
+        public ActionResult GetDeletar(string nomeBanco)
+        {
+            var auxBanco = (from banco in _context.Bancos
+                            where banco.Nome == nomeBanco
+                            select banco).FirstOrDefault();
+
+            _context.Bancos.Remove(auxBanco);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("addRange")]
+        public ActionResult GetAddRange()
+        {
+            _context.AddRange(
+                new Banco { Nome = "Bradesco"},
+                new Banco { Nome = "Caixa" }
+                );
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ MegaworkApi/Migrations/20210304130000_initial.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoWebApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bancos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bancos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BancoID = table.Column<int>(type: "int", nullable: false),
+                    NumeroConta = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Agencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContaAtiva = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contas_Bancos_BancoID",
+                        column: x => x.BancoID,
+                        principalTable: "Bancos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contas_BancoID",
+                table: "Contas",
+                column: "BancoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contas");
+
+            migrationBuilder.DropTable(
+                name: "Bancos");
+        }
+    }
+}
